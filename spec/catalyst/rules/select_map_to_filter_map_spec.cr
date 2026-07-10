4d033d5268bc91329de7400a505bb23551fd57a6
[NSObject init] <49247588+unurgunite@users.noreply.github.com>
2026-07-10 14:57:16 +0300

    [0.2.0] Add CAT-033..034: enumerable fusion rules (#29)

---
+++ spec/catalyst/rules/select_map_to_filter_map_spec.cr
@@ -0,0 +1,54 @@
+require "../../spec_helper"
+
+module Catalyst
+  module Rules
+    describe SelectMapToFilterMap do
+      rule = SelectMapToFilterMap.new
+
+      describe "#check" do
+        it "detects select{}.map{}" do
+          assert_finding(rule, "[1, 2, 3].select { |x| x > 0 }.map { |x| x * 2 }")
+        end
+
+        it "detects select.map (no select block)" do
+          assert_finding(rule, "[1, 2, 3].select.map { |x| x * 2 }")
+        end
+
+        it "detects select(&.positive?).map(&.to_s)" do
+          assert_finding(rule, "[1, 2, 3].select(&.positive?).map(&.to_s)")
+        end
+
+        it "ignores map without select" do
+          assert_no_finding(rule, "[1, 2, 3].map { |x| x * 2 }")
+        end
+
+        it "ignores select without map" do
+          assert_no_finding(rule, "[1, 2, 3].select { |x| x > 0 }")
+        end
+
+        it "ignores reject.map (different inner method)" do
+          assert_no_finding(rule, "[1, 2, 3].reject { |x| x > 0 }.map { |x| x * 2 }")
+        end
+      end
+
+      describe "#id" do
+        it "returns CAT-033" do
+          rule.id.should eq("CAT-033")
+        end
+      end
+
+      describe "#severity" do
+        it "returns info" do
+          rule.severity.should eq("info")
+        end
+      end
+
+      describe "#description" do
+        it "returns description text" do
+          rule.description.should contain("select")
+          rule.description.should contain("map")
+        end
+      end
+    end
+  end
+end
